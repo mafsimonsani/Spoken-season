816f265c8088142b9b8f34f42aeabdf203df7ba6
mafsimonsani <104512807+mafsimonsani@users.noreply.github.com>
2023-12-10 22:51:24 +0100

    Code

---
+++ spoken_season.py
@@ -0,0 +1,8 @@
+# WC '16 Contest 1 J1 - A Spooky Season
+integer_s = int(input())
+
+# given integer_s, cast a spooky incantation on your computer to have it produce the corresponding spooky word
+
+word = 'sp' + (integer_s * 'o') + 'ky'
+
+print(word)
